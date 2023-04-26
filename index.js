bc64584f9fb317144714423b8ac9cce3825dccbc
morrispg <124636880+morrispg@users.noreply.github.com>
2023-04-26 15:26:36 -0500

    readmegenerator update wed

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+	{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a brief description of your project:'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions for your project?'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How should your project be used?'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What are the guidelines for contributing to your project?'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'What tests can be run for your project?'
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'What license is your project under?'
+  }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const fs = require ('fs');
+function writeToFile(fileName, data) {
+	fs.writeFile(filename, content, (err) => {
+    if (err) throw err;
+    console.log('README file created successfully!');
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
+function init() {
+	inquirer.prompt(questions).then(res => console.log(res))
+
+
+
+}
+
 init();
+
+//const filename = 'README.md';
+//const content = '# My README\n\nThis is the content of my README file.';
+//createREADME(filename, content);
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+
+  switch (license.toLowerCase()) {
+    case 'mit':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'apache':
+    case 'apache 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+
+  switch (license.toLowerCase()) {
+    case 'mit':
+      return 'https://opensource.org/licenses/MIT';
+    case 'apache':
+    case 'apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+
+  switch (license.toLowerCase()) {
+    case 'mit':
+      return `## License\n\nThis project is licensed under the [MIT License](${getLicenseLink(license)}).`;
+    case 'apache':
+    case 'apache 2.0':
+      return `## License\n\nThis project is licensed under the [Apache License 2.0](${getLicenseLink(license)}).`;
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.title}\n\n${description}\n\n## Table of Contents\n\n- [Installation](#installation)\n- [Usage](#usage)\n${licenseSection ? '- [License](#license)\n' : ''}\n## Installation\n\n${installation}\n\n## Usage\n\n${usage}\n\n${licenseSection}`;
 }
 
 module.exports = generateMarkdown;
